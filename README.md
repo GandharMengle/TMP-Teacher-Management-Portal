d56bbf080015552a12c094950224af1e3330d196
Gandhar Mengle <gandharmengle@gmail.com>
2024-09-08 04:24:49 +0530

    Added Splash Screen and Login UI

--- README.md
+++ README.md
@@ -7,4 +7,16 @@ The initial ideology is to create an app which will help teachers to perform bel
 1. Add or Delete Organization.
 2. Count number of teaching hours.
 3. Calculate monthly expected payment from each organization
+
+Current Tasks:
+1. Splash screen.
+2. Login screen.
+   a. Login screen have 2 xml files - one for API 24 to 25 and another for API 26 and above
+3. Integration of Firebase authentication.
+4. Home screen.
+
+Fixes Needed:
+1. Unlock the orientation lock for login activity
+   a. Currently screen orientation for login activity is locked to Portrait as UI is designed in linear layout thus in landscape the items not scrollable.
+
 </p>
--- app/src/main/java/com/gandharmengle/teachermanagementportal/LoginActivity.java
+++ app/src/main/java/com/gandharmengle/teachermanagementportal/LoginActivity.java
@@ -1,9 +1,7 @@
 package com.gandharmengle.teachermanagementportal;
 
-import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
-import android.os.Handler;
-import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,33 +9,21 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
-
-    Handler handler;
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
 
-
-            handler = new Handler();
-            handler.postDelayed(new Runnable() {
-                @Override
-                public void run() {
-                    Intent LoginActivity = new Intent(MainActivity.this,com.gandharmengle.teachermanagementportal.LoginActivity.class);
-                    startActivity(LoginActivity);
-                    finish();
-                }
-            }, 4000);
-
+            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+            //Screen orientation is set to PORTRAIT by default is should be free to rotate in further update.
 
             return insets;
-
         });
     }
 }
